52fb34a397694a461f1189c906f25a75bf6ad33a
thisisdanh <thisisdanh@gmail.com>
2022-02-21 14:27:47 -0600

    updatedd mmm

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,115 @@
+var store = [{
+        "title": "Hello, World!",
+        "excerpt":"This is the first post of this blog.   ","categories": [],
+        "tags": [],
+        "url": "/Hello-World/",
+        "teaser": null
+      },{
+        "title": "Single-Cell Notes",
+        "excerpt":"I have been working on single-cell data for almost a year now and I will be using this blog to detail what I have learned along with some tips and tricks. My research focuses on development of mesenchymal tissues and how genes that regulate these processes could be used to...","categories": [],
+        "tags": ["single-cell","list","tutorials","bioinformatics","R"],
+        "url": "/single-cells/",
+        "teaser": null
+      },{
+        "title": "Principal Component Analysis",
+        "excerpt":"In this post, I will be discussing Principal Component Analysis (PCA) and how it can be used in datasets to understand variation with many different variables. Many real-world datasets are often in high dimensions. In other words, they contain so many variables that it is difficult to distinguish a trend....","categories": [],
+        "tags": ["tutorials","bioinformatics","R","machine-learning"],
+        "url": "/PCA/",
+        "teaser": null
+      },{
+        "title": "Learning R",
+        "excerpt":"This is a curated collection of R educational materials for learning R. I will add more as I find them. Books R Bootcamp Learning Statistics with R Efficient R programming An Introduction to Statistical Learning with Applications in R Another Book on Data Science -Data Analysis and Prediction Algorithms with...","categories": [],
+        "tags": ["list","tutorials","bioinformatics","R"],
+        "url": "/Learning-R/",
+        "teaser": null
+      },{
+        "title": "Seurat Guided Clustering Tutorial",
+        "excerpt":"While the vignette on the Seurat website already provides good instructions, I will be using this to give additional thoughts and details that could help beginners to Seurat. In addition, I will provide some recommendations on the workflow as well. Loading the files The first thing the tutorial asks you...","categories": [],
+        "tags": ["single-cell","tutorials","bioinformatics","R"],
+        "url": "/Seurat-Guided-Clustering-Tutorial/",
+        "teaser": null
+      },{
+        "title": "Trajectory Analysis with Diffusion Pseudotime",
+        "excerpt":"The beauty of single-cell RNA-seq is the ability to delineate the cell state of each single-cell. This brings a novel advantage when considering developmental trajectories during organ development or cell differentiation. The reason for this is that biological processes are not always in synchrony. In other words, not all cells...","categories": [],
+        "tags": ["single-cell","tutorials","bioinformatics","R"],
+        "url": "/Diffusion-Pseudotime/",
+        "teaser": null
+      },{
+        "title": "Converting Multiple SRA to FASTQ",
+        "excerpt":"In this post, I will detail how to download some raw sequence data from GEO/SRA in fastq format. We will be using data from the following publication as an example: Rauch A, Haakonsson AK, Madsen JGS, Larsen M et al. Osteogenesis depends on commissioning of a network of stem cell...","categories": [],
+        "tags": ["tutorials","bioinformatics"],
+        "url": "/SRA-conversion/",
+        "teaser": null
+      },{
+        "title": "Preparing an F31",
+        "excerpt":"The Ruth L. Kirschstein Predoctoral Individual National Research Service Award or the F31 is a predoctoral award that provides a stipend and some research allowances for predoctoral students. It is a fellowship provided by the National Institutes of Health (NIH). Preparing an F31 is an arduous task involving many moving...","categories": [],
+        "tags": ["tips"],
+        "url": "/Preparing-an-F31/",
+        "teaser": null
+      },{
+        "title": "Presentation Tips",
+        "excerpt":"Here are some presentation tips that I have used over the years. This post is also for me to put down my thoughts for safekeeping. Tip #1: Who is your audience? Your presentation should be tailored to your audience. Try to think about who it is you are talking to....","categories": [],
+        "tags": ["tips"],
+        "url": "/Presentation-Tips/",
+        "teaser": null
+      },{
+        "title": "Making a Professional Website",
+        "excerpt":"As a student or early-career researcher, you may have seen a trend in professional websites created by your peers. You may have wondered whether or not you, yourself, should make one. In this post, I will give you a couple of reasons why you should make a website and give...","categories": [],
+        "tags": ["tutorials","tips"],
+        "url": "/Making-a-professional-website/",
+        "teaser": null
+      },{
+        "title": "Non-negative Matrix Factorization",
+        "excerpt":"In this post, I will be discussing Non-negative Matrix Factorization (NMF). NMF is a low-rank approximation algorithm that discovers latent features in your data. It is similar to PCA in the sense that they both reduce high-dimensional data into lower dimensions for better understanding of the data. The major difference...","categories": [],
+        "tags": ["tutorials","R","machine-learning"],
+        "url": "/Non-negative-Matrix-Factorization/",
+        "teaser": null
+      },{
+        "title": "Writing the code for PCA",
+        "excerpt":"In this post, we will extend what we did in the previous PCA tutorial by doing it from scratch in R. We will be focusing on Singular Value Decomposition (SVD), which is a tool for matrix factorization. SVD states that any matrix \\(M A^{m n}\\) can be decomposed into the...","categories": [],
+        "tags": ["tutorials","R","machine-learning"],
+        "url": "/Writing-the-code-for-PCA/",
+        "teaser": null
+      },{
+        "title": "Gradient Descents",
+        "excerpt":"Gradient Descent In this post, we are going to discuss Gradient Descents; what it is and how to use it. Gradient Descent is used in many machine learning algorithms to find the local miniumum in differentiable functions where there is a linear relationship. It goes hand in hand with optimization...","categories": [],
+        "tags": ["tutorials","R","machine-learning"],
+        "url": "/Gradient-Descents/",
+        "teaser": null
+      },{
+        "title": "Entropy",
+        "excerpt":"Entropy Entropy is a core concept in Information Theory. It is defined as the average level of ‘information’, ‘surprise’, or ‘uncertainty’ inherent in the possible outcome of a random variable. For example, a coin flip has two possibilities so we can expect it to have lower entropy than a dice,...","categories": [],
+        "tags": ["tutorials","machine-learning","R"],
+        "url": "/Entropy/",
+        "teaser": null
+      },{
+        "title": "Text mining using TidyText",
+        "excerpt":"Introduction Text mining is a process of discovering new and latent features within a body of text. It uses Natural Language Processing (NLP) to enable computers to digest human language. It has many uses in the real world. A couple of examples included are: Spam Filtering Sentiment Analysis Fraud Detection...","categories": [],
+        "tags": ["machine-learning","tutorials","R"],
+        "url": "/Text-mining-using-TidyText/",
+        "teaser": null
+      },{
+        "title": "Exploratory Data Analysis in R",
+        "excerpt":"In this post, we will be analyzing data as if we were looking at it for the first time. This is called ‘Exploratory Data Analysis’. We will visualize the data and gather insights from the iris data set. Exploring the data First, let’s load up the data and have a...","categories": [],
+        "tags": ["tutorials","bioinformatics","R","machine-learning"],
+        "url": "/Exploratory-Data-Analysis-in-R/",
+        "teaser": null
+      },{
+        "title": "Conditional Probability with R",
+        "excerpt":"In this post, we will explore conditional probability and how it relates to Bayes’ Theorem. Conditional Probability Conditional probability is the likelihood of an event or outcome occurring based on the occurrence of a previous event or outcome. Below is the equation for the conditional probability. \\(P(A|B) = \\frac{P(A \\cap...","categories": [],
+        "tags": ["tutorials","R"],
+        "url": "/Conditional-Probability-with-R/",
+        "teaser": null
+      },{
+        "title": "K-means from scratch in R",
+        "excerpt":"K-means is an unsupervised machine learning clustering algorithm. It can be used to cluster a set of observations based on similarity between the observations. K-means is one of the most popular clustering technique and it is quite simple to understand. K-means from scratch in R K-means clustering algorithm The goal...","categories": [],
+        "tags": ["bioinformatics","tips","tutorials","machine-learning"],
+        "url": "/K-means-from-scratch-in-R/",
+        "teaser": null
+      },{
+        "title": "Introduction to R and RStudio",
+        "excerpt":"In this post, we will introduce R and RStudio. We will learn how to use R and interact with RStudio. What is R? R is a language and environment for statistical computing and graphics. It is widely used for a variety of statistical analysis (i.e., linear and nonlinear modeling, classical...","categories": [],
+        "tags": ["bioinformatics","tips","tutorials"],
+        "url": "/Introduction-to-R-and-RStudio/",
+        "teaser": null
+      }]
