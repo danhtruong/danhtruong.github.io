28c5b84e0d9d805db109ec4dc8af62544e819f57
thisisdanh <thisisdanh@gmail.com>
2022-02-22 09:49:28 -0600

    icons and div

--- assets/scripts/copyCode.js
+++ assets/scripts/copyCode.js
@@ -1,5 +1,5 @@
 // assets/js/post.js
-var codeBlocks = document.querySelectorAll('pre.highlight');
+var codeBlocks = document.querySelectorAll('div.language-r highlighter-rouge');
 
 codeBlocks.forEach(function (codeBlock) {
   var copyButton = document.createElement('button');
