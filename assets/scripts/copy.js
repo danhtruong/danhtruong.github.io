94ac36092e1ac75941a9bdce91731036f2f6e68f
Danh Truong <thisisdanh@gmail.com>
2022-03-02 10:08:29 -0600

    Update copy.js
    added copy for js and bash

--- assets/scripts/copy.js
+++ assets/scripts/copy.js
@@ -1,4 +1,4 @@
-codes = document.querySelectorAll('div.language-r > div > pre.highlight > code, div.language-sh > div > pre.highlight > code');
+codes = document.querySelectorAll('div.language-r > div > pre.highlight > code, div.language-sh > div > pre.highlight > code, , div.language-bash > div > pre.highlight > code, , div.language-js > div > pre.highlight > code');
 countID = 0;
 codes.forEach((code) => {
 
